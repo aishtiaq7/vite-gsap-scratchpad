87464a95fb9647d77d9cfcb715aab9120615d4d3
aishtiaq <awshaf_ishtiaque@sfu.ca>
2024-04-12 04:39:17 -0700

    t2 starts after t1 finishes

--- src/App.jsx
+++ src/App.jsx
@@ -10,27 +10,63 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 function App() {
-
   const container = useRef();
   const boxRef = useRef();
+  const circleRef = useRef();
   useGSAP(
     () => {
       const t1 = gsap.timeline({
-        onComplete: () => {
-          //
-        },
-      });
-      t1.from(boxRef.current, {
         scrollTrigger: {
           markers: true,
           trigger: boxRef.current,
           start: "top center", // Adjust these values based on your layout
-          end: "bottom top",
-          scrub: true, // Optional: makes the animation smooth on scroll
+          end: "bottom 20%",
+          scrub: 2, // Optional: makes the animation smooth on scroll
         },
+        onComplete: () => {
+          console.log('t1 completed')
+          t2.restart();
+        },
+      });
+      t1.from(boxRef.current, {
         rotate: 360,
         y: -200,
         duration: 3,
+      })
+        .to(boxRef.current, {
+          rotate: 0,
+          x: -200,
+          duration: 3,
+        })
+        .to(boxRef.current, {
+          rotate: 0,
+          x: 0,
+          y: 0,
+          backgroundColor: "red",
+          duration: 3,
+        });
+
+      // Second timeline for the circle
+      // const t2 = gsap.timeline({
+      //   scrollTrigger: {
+      //     markers: true,
+      //     trigger: circleRef.current,
+      //     start: "top 80%",
+      //     end: "top 20%",
+      //     scrub: 1,
+      //   },
+      //   paused: true,
+      // });
+      const t2 = gsap.timeline({ paused: true });
+      t2.to(circleRef.current, {
+        x: 300,
+        rotation: 180,
+        duration: 2,
+      })
+      .to(circleRef.current, {
+        x: 0,
+        rotation: 180,
+        duration: 2,
       });
     },
     {
@@ -43,11 +79,10 @@ function App() {
   return (
     <>
       <div ref={container}>
-        <section className="section">
-        </section>
+        <section className="section"></section>
         <section className="section">
           <div ref={boxRef} className="box"></div>
-          <div className="circle"></div>
+          <div ref={circleRef} className="circle"></div>
         </section>
         <section className="section"></section>
       </div>
